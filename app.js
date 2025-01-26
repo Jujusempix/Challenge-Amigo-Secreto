c03fcd0ae9a0941733597956e80ef473bee0839f
Jujusempix <julio.guilherme@grupointegrado.br>
2025-01-26 15:16:35 -0300

    V1.0 - sistema de sorteio completo

--- app.js
+++ app.js
@@ -6,30 +6,37 @@ function adicionarAmigo(nome) {
         alert('Por favor, insira um nome');
     } else {
         listaAmigos.push(nome);
+        limpaTudo('resultado');
     }
-    limpaCampo();
-    exibirTextoNaTela(nome);
-    console.log(listaAmigos);
-    console.log(nome);
+    limpaTudo('amigo');
+    exibirAmigos(nome);
 }
 
-function exibirTextoNaTela(texto){
-    let campo = document.querySelector('ul').appendChild(document.createElement('li'));
+function exibirAmigos(texto){
+    let campo = document.getElementById('listaAmigos').appendChild(document.createElement('li'));
     campo.innerHTML = texto;
 }
 
-function limpaCampo() {
-    campo = document.getElementById('amigo');
+function exibirSorteado(texto) {
+    campo = document.getElementById('resultado').appendChild(document.createElement('li'));
+    campo.innerHTML = texto;
+}
+
+function limpaTudo(tag) {
+    campo = document.getElementById(tag);
+    campo.innerHTML = '';
     campo.value = '';
 }
 
-function limpaAmigos() {
-    campo = document.getElementById('listaAmigos');
-    campo.innerHTML = ''
-}
-
 function sortearAmigo() {
-    let sorteio =  parseInt(Math.random() * listaAmigos.length + 1);
-    limpaAmigos()
-    exibirTextoNaTela(sorteio)
+    let sorteio =  parseInt(Math.random() * listaAmigos.length);
+    
+    if(listaAmigos[sorteio] == undefined){
+        limpaTudo('resultado');
+        alert('Não há nomes para sortear, por favor, insira novos nomes');
+    } else {
+        limpaTudo('listaAmigos');
+        exibirSorteado(`O nome sorteado foi: ${listaAmigos[sorteio]}`);
+        listaAmigos.length = 0
+    }
 }
